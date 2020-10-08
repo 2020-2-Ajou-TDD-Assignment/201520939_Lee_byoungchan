76fe1da35cd1713d5c24411d0bd10297c8fd5879
Lee Byoungchan <dinitic@ajou.ac.kr>
2020-10-09 04:51:57 +0900

    testOneStrike is added and implemented

--- BowlingGame/src/Game.java
+++ BowlingGame/src/Game.java
@@ -12,12 +12,16 @@ public class Game {
 		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++){
 			
-			if (isSpare(frameIndex)){
-				score += 10 + rolls[frameIndex + 2];
+			if (isStrike(frameIndex)){
+				score += 10 + strikeBonus(frameIndex);
+				frameIndex++;
+			}
+			else if (isSpare(frameIndex)){
+				score += 10 + spareBonus(frameIndex);
 				frameIndex += 2;
 			} 
 			else {
-				score += rolls[frameIndex] + rolls[frameIndex+1];
+				score += sumOfBallsInFrame(frameIndex);
 				frameIndex += 2;
 			}
 				
@@ -25,8 +29,24 @@ public class Game {
 		return score;
 	}
 
-	private boolean isSpare(int frameIndex) {
-		return rolls[frameIndex] + rolls[frameIndex+ 1] == 10;
+	
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+	
+	private int sumOfBallsInFrame(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1];
 	}
 
+	private int spareBonus(int frameIndex) {
+		return rolls[frameIndex + 2];
+	}
+	
+	private int strikeBonus(int frameIndex) {
+		return rolls[frameIndex+1] + spareBonus(frameIndex);
+	}
+	
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
+	}
 }
